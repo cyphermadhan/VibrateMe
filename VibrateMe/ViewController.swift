5edbd1cdb7e866110cb8dc008b99a5dc8c5ff2cf
Madhan <maddy.ax@gmail.com>
2021-07-04 17:38:11 +0530

    Added Ripple styled button

--- VibrateMe/ViewController.swift
+++ VibrateMe/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Madhan Raj on 01/07/21.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -67,6 +66,8 @@ class ViewController: UIViewController {
         let generator = UIImpactFeedbackGenerator(style: .heavy)
         generator.impactOccurred()
     }
+        
+    
     
     
     
@@ -74,6 +75,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonMedium: UIButton!
     @IBOutlet weak var buttonHeavy: UIButton!
     @IBOutlet weak var vibrateLogo: UIImageView!
+    
+    //Calling an outlet for the button to be a rippleable button
+    @IBOutlet var rippleButton: RippleableButton!
+    
     
     
     override func viewDidLoad() {
@@ -84,12 +89,29 @@ class ViewController: UIViewController {
         buttonLight.layer.cornerRadius = 12
         buttonMedium.layer.cornerRadius = 12
         buttonHeavy.layer.cornerRadius = 12
+       
         
+        //Defining the ripple button colour and type
+        rippleButton.type = .contained
+        rippleButton.primaryColor = UIColor(named: "buttonColourPurple")
+        rippleButton.layer.cornerRadius = 12
+       
+//      This button part is working - calling the button directly in code
         
-        
+//        let outLinedButton: RippleableButton = {
+//          let button = RippleableButton(type: .outlined,frame: CGRect(x: 200, y: 100, width: 100, height: 50))
+//          button.layer.cornerRadius = 5
+//          button.setTitle("Hello World!", for: .normal)
+//          button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+//          button.primaryColor = UIColor.blue
+//          return button
+//        }()
+//
+
         
     }
-
+    
+    
 
 }
 
